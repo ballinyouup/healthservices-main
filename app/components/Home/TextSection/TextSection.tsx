fdbd7ff7ff8f9cfe80907e4b52bf8601f7e4ce31
ballinyouup <bryanhuici@gmail.com>
2023-04-15 19:48:01 -0400

    added text section

--- app/components/Home/TextSection/TextSection.tsx
+++ app/components/Home/TextSection/TextSection.tsx
@@ -1,19 +1,34 @@
+"use client";
+import { motion } from "framer-motion";
 const TextSection = () => {
 	return (
 		<section className="w-full">
-			<div className="flex h-48 items-center justify-center">
-				<div className="flex w-full max-w-xl items-center justify-center gap-4 p-2">
-					<span className="w-40 text-end text-4xl font-bold uppercase text-purple">
+			<div className="flex items-center justify-center sm:h-48">
+				<motion.div
+					initial={{
+						opacity: 0,
+					}}
+					animate={{
+						opacity: 1,
+					}}
+					transition={{
+						delay: 0.75,
+						duration: 0.75,
+						ease: "easeIn",
+					}}
+					className="flex w-full max-w-xl flex-col items-start justify-center gap-4 p-8 sm:flex-row sm:items-center sm:p-2"
+				>
+					<span className="w-40 text-start text-4xl font-bold uppercase text-purple sm:text-end">
 						Why Choose Us
 					</span>
-					<span className="h-28 border-[1.5px] border-black" />
+					<span className="w-full border-[1.5px] border-black sm:h-28 sm:w-fit" />
 					<span>
 						Our plans not only offer comprehensive coverage for routine checkups
 						and major medical procedures but our agents also provide
 						personalized attention and guidance to help you find the plan that's
 						right for you.
 					</span>
-				</div>
+				</motion.div>
 			</div>
 		</section>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,15 @@ import { Inter } from "next/font/google";
 import Hero from "./components/Home/Hero/Hero";
 import HomeCardSection from "./components/Home/CardSection/HomeCardSection";
 import TextSection from "./components/Home/TextSection/TextSection";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "HealthServicesFL.com",
+	description:
+		"At HealthServicesFL.com, we understand the importance of good health and financial security. That's why we offer a range of insurance options, including Obamacare, Medicare, Funeral Assistance, and Life Insurance. Our commitment to excellence in customer service means you can trust us to take care of all your insurance needs.",
+};
 
 export default function Home() {
 	return (
