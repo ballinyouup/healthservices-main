fa39549395396edabb54a4631d25f3e253e704db
ballinyouup <bryanhuici@gmail.com>
2023-04-16 22:59:20 -0400

    x

--- app/components/Home/ContactSection/ContactSection.tsx
+++ app/components/Home/ContactSection/ContactSection.tsx
@@ -2,13 +2,13 @@ import ContactCard from "./ContactCard";
 import Image from "next/image";
 const ContactSection = () => {
 	return (
-		<section className="relative flex h-full w-full flex-row items-center justify-center gap-20 bg-purpleDark p-20">
+		<section className="relative flex h-full w-full flex-col-reverse items-center justify-center gap-20 bg-purpleDark px-8 py-20 lg:flex-row">
 			<Image
 				src="/family.png"
 				alt="Family Image"
 				width={500}
 				height={500}
-				className="mix-blend-luminosity"
+				className="hidden mix-blend-luminosity lg:block"
 				unoptimized
 			/>
 			<ContactCard text="Contact US" />
