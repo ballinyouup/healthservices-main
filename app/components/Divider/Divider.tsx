01fdc0b79a899b6c851964f55fcebf9639e1c99d
ballinyouup <bryanhuici@gmail.com>
2023-04-18 22:50:48 -0400

    added another divider and fixed divider styling

--- app/components/Divider/Divider.tsx
+++ app/components/Divider/Divider.tsx
@@ -1,7 +1,7 @@
 const Divider = () => {
 	return (
-		<div className="flex h-12 w-full items-center justify-center px-8">
-			<hr className="w-full max-w-2xl border border-purpleDark lg:max-w-5xl" />
+		<div className="flex h-12 w-full items-center justify-center px-8 lg:px-0">
+			<hr className="w-full max-w-2xl border border-purpleDark border-opacity-30 lg:max-w-7xl" />
 		</div>
 	);
 };
--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
 			<div className="flex h-full w-full flex-col md:max-w-9xl">
 				<Hero />
 				<TextSection />
+				<Divider />
 				<HomeCardSection />
 				<WhySection />
 				<AboutSection />
