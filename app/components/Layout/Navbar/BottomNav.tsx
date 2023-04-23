fdaea11cd2c9761d759c68fa72a3b8778d9fa232
ballinyouup <bryanhuici@gmail.com>
2023-04-23 11:30:03 -0400

    fixed nav links

--- app/components/Layout/Navbar/BottomNav.tsx
+++ app/components/Layout/Navbar/BottomNav.tsx
@@ -2,15 +2,20 @@ import Button from "../../Button/Button";
 import MenuDropdown from "./Menu/MenuDropdown";
 import Logo from "./Logo";
 import MobileMenu from "./MobileMenu/MobileMenu";
+import Link from "next/link";
 
 const BottomNav = () => {
 	return (
 		<div className="flex h-16 w-full flex-row items-center justify-between bg-purple px-4 py-10">
 			<Logo />
 			<div className="hidden w-fit gap-2 sm:flex">
-				<Button clear>Home</Button>
+				<Button clear>
+					<Link href="/">Home</Link>
+				</Button>
 				<MenuDropdown />
-				<Button clear>Contact Us</Button>
+				<Button clear>
+					<Link href="/">Contact Us</Link>
+				</Button>
 			</div>
 			<MobileMenu />
 		</div>
